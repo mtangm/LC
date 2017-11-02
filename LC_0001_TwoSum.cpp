04756d967130899d7baced8b3cea41878fc9f23b
mtangm <33326717+mtangm@users.noreply.github.com>
2017-11-02 12:13:15 -0700

    TwoSum, 4 weeks ago

--- LC_0001_TwoSum.cpp
+++ LC_0001_TwoSum.cpp
@@ -1,1 +1,26 @@
-#include
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &numbers, int target) {
+        map<int, int> M;//value_index_map;
+        int index1=0;
+        int index2 = 0;
+        vector<int> res;
+        
+        for (int i = 0; i < numbers.size(); i++)
+        {
+            if (M.find(target - numbers[i]) != M.end())
+            {
+                index1=M[target-numbers[i]];
+                index2=i;
+                res.push_back(index1);
+                res.push_back(index2);
+                break;
+            }
+
+            M[numbers[i]] = i;
+        }
+        
+        return res;       
+        
+    }
+};
